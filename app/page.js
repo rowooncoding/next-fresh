384e074fa07450a4ec03295ce7d98dd06b128418
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-17 17:38:49 +0900

    [docs] 네비 바 만들기 [feat] link 연결

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
+      <div className="navbar">
+        <Link href="/">홈</Link>
+        <Link href="/list">List</Link>
+      </div>
       <h4 className="title">애플 후레시</h4>
       <p className="title-sub">by dev rowoon</p>
     </div>
