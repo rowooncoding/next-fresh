e0dfd676fd8a01eec5f16133a869efd8058f39d8
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-17 18:50:59 +0900

    [feat] map을 사용하여 동적으로 랜더링

--- app/list/page.js
+++ app/list/page.js
@@ -4,15 +4,13 @@ export default function List() {
   return (
     <div>
       <h2 className="title">Products</h2>
-      <div className="food">
-        <h4>{products[0]} $40</h4>
-      </div>
-      <div className="food">
-        <h4>{products[1]} $40</h4>
-      </div>
-      <div className="food">
-        <h4>{products[2]} $40</h4>
-      </div>
+      {products.map(() => {
+        return (
+          <div className="food">
+            <h4>{products[0]} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
