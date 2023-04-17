56b861656bc0c320f5889d81b2bbb70a45035637
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-17 19:21:12 +0900

    [feat] 백틱과 달러를 사용하여 각각 다른 이미지 보여주기

--- app/list/page.js
+++ app/list/page.js
@@ -9,7 +9,7 @@ export default function List() {
       {products.map((item, i) => {
         return (
           <div className="food" key={i}>
-            <img src="/food0.png" className="food-img" />
+            <img src={`/food${i}.png`} className="food-img" />
             <h4>{products[i]} $40</h4>
           </div>
         );
