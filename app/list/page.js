3f741205ffc2b89ad6426afbf62f4c4e347fbf49
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-17 19:19:17 +0900

    [docs] import 삭제하고 원래대로 돌려놓기

--- app/list/page.js
+++ app/list/page.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import tomato from "/public/food0.png";
 
 export default function List() {
   //db데이터를 가져왔다고 생각함
@@ -10,7 +9,7 @@ export default function List() {
       {products.map((item, i) => {
         return (
           <div className="food" key={i}>
-            <Image src={tomato} className="food-img" />
+            <img src="/food0.png" className="food-img" />
             <h4>{products[i]} $40</h4>
           </div>
         );
