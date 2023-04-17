20d59288832ec6a010e7e8036a32358f760863d6
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-17 18:45:49 +0900

    [docs] 리스트 페이지 하드코딩하기

--- app/list/page.js
+++ app/list/page.js
@@ -1,12 +1,17 @@
 export default function List() {
+  //db데이터를 가져왔다고 생각함
+  let products = ["Tomatoes", "Pasta", "Coconut"];
   return (
     <div>
       <h2>Products</h2>
       <div className="food">
-        <h4>상품명 $40</h4>
+        <h4>{products[0]} $40</h4>
       </div>
       <div className="food">
-        <h4>상품명 $40</h4>
+        <h4>{products[1]} $40</h4>
+      </div>
+      <div className="food">
+        <h4>{products[2]} $40</h4>
       </div>
     </div>
   );
