15f650ae70992cac7b2c61e89ee92b346534b130
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-17 17:45:27 +0900

    [docs] 공통 컴포넌트는 layout 컴포넌트에다가 만들자

--- app/page.js
+++ app/page.js
@@ -7,10 +7,6 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
-      <div className="navbar">
-        <Link href="/">홈</Link>
-        <Link href="/list">List</Link>
-      </div>
       <h4 className="title">애플 후레시</h4>
       <p className="title-sub">by dev rowoon</p>
     </div>
