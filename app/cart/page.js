0003063e17bbb22a943192306e336d118c6f3cad
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-18 13:23:34 +0900

    [feat] import 문법 익혀보기

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,9 +1,11 @@
+import { age, name } from "./data.js";
+
 export default function Cart() {
   return (
     <div>
       <h4 className="title">Cart</h4>
       <div className="cart-item">
-        <p>상품명</p>
+        <p>상품명 {age}</p>
         <p>$40</p>
         <p>1개</p>
       </div>
