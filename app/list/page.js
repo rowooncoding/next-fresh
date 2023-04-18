0483a27677b3241444a925e3d3d2f272782a8570
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-18 15:28:50 +0900

    [feat] 버튼 누르면 +, - 가능하도록 설정

--- app/list/page.js
+++ app/list/page.js
@@ -6,6 +6,14 @@ export default function List() {
   //db데이터를 가져왔다고 생각함
   let products = ["Tomatoes", "Pasta", "Coconut"];
   const [count, setCount] = useState(0);
+
+  const onIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const onDecrease = () => {
+    setCount(count - 1);
+  };
   return (
     <div>
       <h2 className="title">Products</h2>
@@ -14,21 +22,9 @@ export default function List() {
           <div className="food" key={i}>
             <img src={`/food${i}.png`} className="food-img" />
             <h4>{item} $40</h4>
-            <button
-              onClick={() => {
-                setCount(count - 1);
-              }}
-            >
-              -
-            </button>
+            <button onClick={onDecrease}>-</button>
             <span> {count} </span>
-            <button
-              onClick={() => {
-                setCount(count + 1);
-              }}
-            >
-              +
-            </button>
+            <button onClick={onIncrease}>+</button>
           </div>
         );
       })}
