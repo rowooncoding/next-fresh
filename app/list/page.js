e74fe09e768079017f51f3df27eaa669918590b2
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-18 14:24:25 +0900

    [feat] 버튼 누르면 +1이 되는 useState훅 공부하기

--- app/list/page.js
+++ app/list/page.js
@@ -1,8 +1,11 @@
 "use client";
 
+import { useState } from "react";
+
 export default function List() {
   //db데이터를 가져왔다고 생각함
   let products = ["Tomatoes", "Pasta", "Coconut"];
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h2 className="title">Products</h2>
@@ -11,8 +14,14 @@ export default function List() {
           <div className="food" key={i}>
             <img src={`/food${i}.png`} className="food-img" />
             <h4>{item} $40</h4>
-            <span> 1 </span>
-            <button>+</button>
+            <span> {count} </span>
+            <button
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
+              +
+            </button>
           </div>
         );
       })}
