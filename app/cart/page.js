b725a957768e51ce366d2334dab2c9f0c16ac553
Rowoon Lee <dev.rowoon@gmail.com>
2023-04-18 13:35:08 +0900

    [docs] props연결

--- app/cart/page.js
+++ app/cart/page.js
@@ -4,16 +4,16 @@ export default function Cart() {
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <CartItem />
-      <CartItem />
+      <CartItem cart={cart[0]} />
+      <CartItem cart={cart[1]} />
     </div>
   );
 }
 
-function CartItem() {
+function CartItem(props) {
   return (
     <div className="cart-item">
-      <p>상품명</p>
+      <p>{props.cart}</p>
       <p>$40</p>
       <p>1개</p>
     </div>
